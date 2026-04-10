651a236f8b95fb67a4133534105855b386db98ad
Ngatour Rohman <ngatourrohman25@gmail.com>
2026-04-10 22:00:33 +0700

    feat: expand PWA manifest icon spectrum to include all standard sizes

--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,43 @@ export default defineConfig({
         icons: [
           {
             src: 'icons/favicon-app.png',
+            sizes: '72x72',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'icons/favicon-app.png',
+            sizes: '96x96',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'icons/favicon-app.png',
+            sizes: '128x128',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'icons/favicon-app.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'icons/favicon-app.png',
+            sizes: '152x152',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'icons/favicon-app.png',
             sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'icons/favicon-app.png',
+            sizes: '384x384',
             type: 'image/png',
             purpose: 'any',
           },
